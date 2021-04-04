cf2bd2e23994ebb720a35b24804ad285cc1d6a32
marsou001 <marsou431@gmail.com>
2021-04-04 09:50:18 +0100

    fix: move methods outside contructor

--- DataStructures/Set/index.js
+++ DataStructures/Set/index.js
@@ -1,83 +1,83 @@
 class Set {
     constructor() {
         this.collection = [];
+    }
+    
+    values() {
+        return this.collection;
+    }
 
-        this.values = function() {
-            return this.collection;
+    has(element) {
+        return this.collection.indexOf(element) !== -1;
+    }
+
+    add(element) {
+        if (!this.has(element)) {
+            this.collection.push(element);
+            return true;
         }
+        return false;
+    }
 
-        this.has = function(element) {
-            return this.collection.indexOf(element) !== -1;
+    remove(element) {
+        if (this.has(element)) {
+            const index = this.collection.indexOf(element);
+            this.collection.splice(index, 1);
+            return true;
         }
+        return false;
+    }
 
-        this.add = function(element) {
-            if (!this.has(element)) {
-                this.collection.push(element);
-                return true;
+    size() {
+        return this.collection.length;
+    }
+
+    union(otherSet) {
+        const unionSet = new Set();
+        const firstSet = this.values();
+        const secondSet = otherSet.values();
+
+        firstSet.forEach(function(element) {
+            unionSet.add(element);
+        });
+
+        secondSet.forEach(function(element) {
+            unionSet.add(element);
+        });
+
+        return unionSet;
+    }
+
+    intersection(otherSet) {
+        const intersectionSet = new Set();
+        const firstSet = this.values();
+        
+        firstSet.forEach(function(elemenet) {
+            if (otherSet.has(elemenet)) {
+                intersectionSet.add(elemenet)
             }
-            return false;
-        }
+        });
 
-        this.remove = function(element) {
-            if (this.has(element)) {
-                const index = this.collection.indexOf(element);
-                this.collection.splice(index, 1);
-                return true;
+        return intersectionSet;
+    }
+
+    difference(otherSet) {
+        const differenceSet = new Set();
+        const firstSet = this.values();
+
+        firstSet.forEach(function(element) {
+            if (!otherSet.has(element)) {
+                differenceSet.add(element);
             }
-            return false;
-        }
+        });
 
-        this.size = function() {
-            return this.collection.length;
-        }
+        return differenceSet;
+    }
 
-        this.union = function(otherSet) {
-            const unionSet = new Set();
-            const firstSet = this.values();
-            const secondSet = otherSet.values();
-
-            firstSet.forEach(function(element) {
-                unionSet.add(element);
-            });
-
-            secondSet.forEach(function(element) {
-                unionSet.add(element);
-            });
-
-            return unionSet;
-        }
-
-        this.intersection = function(otherSet) {
-            const intersectionSet = new Set();
-            const firstSet = this.values();
-            
-            firstSet.forEach(function(elemenet) {
-                if (otherSet.has(elemenet)) {
-                    intersectionSet.add(elemenet)
-                }
-            });
-
-            return intersectionSet;
-        }
-
-        this.difference = function(otherSet) {
-            const differenceSet = new Set();
-            const firstSet = this.values();
-
-            firstSet.forEach(function(element) {
-                if (!otherSet.has(element)) {
-                    differenceSet.add(element);
-                }
-            });
-
-            return differenceSet;
-        }
-
-        this.subSet = function(otherSet) {
-            const firstSet = this.values();
-            return firstSet.every(function(element) {
-                return otherSet.has(element);
-            });
-        }
+    subSet(otherSet) {
+        const firstSet = this.values();
+        return firstSet.every(function(element) {
+            return otherSet.has(element);
+        });
     }
 }
