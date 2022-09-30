fef6b982ad129fc623e3f2a6210a827dccfe8920
marouane <marsou431@gmail.com>
2022-09-30 11:40:54 +0100

    feat: queue typescript

---
+++ DataStructures/Queue/index.ts
@@ -0,0 +1,43 @@
+class Queue<T> {
+  collection: T[];
+
+  constructor() {
+    this.collection = [];
+  }
+  
+  enqueue(element): void {
+    this.collection.unshift(element);
+  }
+
+  dequeue(): T | null {
+    if (this.isEmpty()) return null;
+    
+    return this.collection.pop()!;
+  }
+
+  front(): T | null {
+    if (this.isEmpty()) return null;
+
+    return this.collection[this.collection.length - 1];
+  }
+
+  back(): T | null {
+    if (this.isEmpty()) return null;
+
+    return this.collection[0];
+  }
+
+  show(): T[] {
+    return this.collection;
+  }
+
+  size(): number {
+    return this.collection.length;
+  }
+
+  isEmpty(): boolean {
+    return this.collection.length === 0;
+  }
+}
+
+export default Queue;
