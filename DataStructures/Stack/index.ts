2b6268a76a25b0cc2209b6baa8b941a888fdca0c
marouane <marsou431@gmail.com>
2022-09-30 11:31:17 +0100

    feat: stack typescript

---
+++ DataStructures/Stack/index.ts
@@ -0,0 +1,35 @@
+class Stack<T> {
+  count: number;
+  storage: Record<number, T>;
+
+  constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
+
+  push(value: T) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop(): T | null {
+    if (this.count === 0) return null;
+
+    this.count--;
+    const result = this.storage[this.count];        
+    delete this.storage[this.count];
+    return result;
+  }
+
+  peek(): T | null {
+    if (this.count === 0)  return null; 
+
+    return this.storage[this.count - 1];
+  }
+
+  size(): number {
+    return this.count;
+  }
+}
+
+export default Stack;
